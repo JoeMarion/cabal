128932b71a6a9b268fd3e0d56d537546792703e8
JoeMarion <joeyzmarion@gmail.com>
2015-12-15 22:31:14 -0800

    Link CSS files to Rails app

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   resources :home
+  resources :network
 
   # You can have the root of your site routed with "root"
   root 'home#index'
